44d4cd55be3b9088d3ebe00f7d5993b36695a379
SCelso <sergio.celso.sanchez@gmail.com>
2022-11-24 16:40:01 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.nebrija.gestorgaleriaarte {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.nebrija.gestorgaleriaarte to javafx.fxml;
+    exports com.nebrija.gestorgaleriaarte;
+}
